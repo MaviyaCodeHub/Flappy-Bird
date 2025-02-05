448b771f671b9342b00f554bc2adfc78a57e35d0
Abhinav Malviya <abhinavmalviya4321@gmail.com>
2025-02-06 02:28:30 +0530

    add assets and states

---
+++ core/src/main/java/com/flappy_bird/game/States/GameStateManager.java
@@ -0,0 +1,13 @@
+package com.flappy_bird.game.States;
+
+import java.awt.Taskbar;
+import java.util.Stack;
+
+import javax.swing.plaf.nimbus.State;
+
+public class GameStateManager {
+    private Stack<State> states;
+    public  GameStateManager(){
+
+    }
+}
--- core/src/main/java/com/flappy_bird/game/States/States.java
+++ core/src/main/java/com/flappy_bird/game/States/States.java
@@ -4,18 +4,18 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector4;
 
-public class States {
-//    protected OrthographicCamera cam;
-//    protected Vector4 mouse;
-//    protected GameStateManager gsm;
-//    public States(GameStateManager gsm) {
-//        this.gsm = gsm;
-//        cam = new OrthographicCamera();
-//        mouse = new Vector4();
-//
-//    }
-//    protected abstract void  handleInput();
-//    public abstract void update(float dt);
-//    public abstract void render(SpriteBatch ab);
-//    public abstract void dispose();
+public abstract class States {
+    protected OrthographicCamera cam;
+    protected Vector4 mouse;
+    protected GameStateManager gsm;
+    protected States(GameStateManager gsm) {
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector4();
+
+    }
+    protected abstract void  handleInput();
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch ab);
+    public abstract void dispose();
 }
